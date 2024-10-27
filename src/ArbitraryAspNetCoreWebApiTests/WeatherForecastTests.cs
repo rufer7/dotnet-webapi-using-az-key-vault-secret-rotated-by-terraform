604696ab686e9d8b39058e3b26ef7b36fe9f4686
rufer7 <rufermarc@gmail.com>
2024-10-27 21:30:45 +0100

    Create xUnit test project including sample unit test
    #2

---
+++ src/ArbitraryAspNetCoreWebApiTests/WeatherForecastTests.cs
@@ -0,0 +1,26 @@
+﻿using ArbitraryAspNetCoreWebApi;
+
+namespace ArbitraryAspNetCoreWebApiTests
+{
+    public class WeatherForecastTests
+    {
+        [Theory]
+        [InlineData(0, 32)]
+        [InlineData(100, 212)]
+
+        public void TemperatureF_ForTemperatureC_ReturnsExpectedValue(int temperatureC, int expectedResultInF)
+        {
+            // Arrange
+            var weatherForecast = new WeatherForecast
+            {
+                TemperatureC = temperatureC
+            };
+
+            // Act
+            var temperatureF = weatherForecast.TemperatureF;
+            
+            // Assert
+            Assert.Equal(expectedResultInF, temperatureF);
+        }
+    }
+}
