ca25126394b38d87c0adc650f96833d9b4a37338
rufer7 <rufermarc@gmail.com>
2024-10-29 21:00:55 +0100

    minor

--- src/ArbitraryAspNetCoreWebApiTests/WeatherForecastTests.cs
+++ src/ArbitraryAspNetCoreWebApiTests/WeatherForecastTests.cs
@@ -6,7 +6,7 @@ namespace ArbitraryAspNetCoreWebApiTests
     {
         [Theory]
         [InlineData(0, 32)]
-        [InlineData(100, 212)]
+        [InlineData(100, 211)]
 
         public void TemperatureF_ForTemperatureC_ReturnsExpectedValue(int temperatureC, int expectedResultInF)
         {
