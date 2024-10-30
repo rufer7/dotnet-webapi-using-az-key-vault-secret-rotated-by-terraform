86cabc3a64e0c304490f0e0848daa5c3ac56e3e4
rufer7 <rufermarc@gmail.com>
2024-10-30 09:28:51 +0100

    IaC - Add terraform configuration
    #3

---
+++ iac/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "~> 1.9.8"
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.0.2"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.7.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.3"
+    }
+  }
+}
