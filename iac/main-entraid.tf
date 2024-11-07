ac32621f7a366fbec9731780ccd0df7f3355579f
Marc Rufer <rufer7@users.noreply.github.com>
2024-11-07 08:59:01 +0100

    Update main-entraid.tf
    Add scope

--- iac/main-entraid.tf
+++ iac/main-entraid.tf
@@ -1,10 +1,23 @@
 # App Registration with client secret
+resource "random_uuid" "random-uuid-forecast-read" {}
+
 resource "azuread_application" "aadapp" {
   display_name     = format("%s Application %s", var.resource_prefix, var.stage)
   identifier_uris  = []
   sign_in_audience = "AzureADMyOrg"
   api {
     requested_access_token_version = 2
+
+    oauth2_permission_scope {
+      admin_consent_description  = "Allows the application to read weather forecast data"
+      admin_consent_display_name = "Read forecast data"
+      enabled                    = true
+      id                         = random_uuid.random-uuid-forecast-read.result
+      type                       = "User"
+      user_consent_description   = "Allows the application to read weather forecast data"
+      user_consent_display_name  = "Read forecast data"
+      value                      = "Forecast.Read"
+    }
   }
 }
 
