cbab03a63590b3841143aed4ed1de1dd95a322d0
rufer7 <rufermarc@gmail.com>
2024-11-07 11:09:48 +0100

    add app reg and service principal for postman to test the API

--- iac/main-entraid.tf
+++ iac/main-entraid.tf
@@ -36,3 +36,43 @@ resource "azurerm_key_vault_secret" "aadapppwd-secret" {
   name         = "AzureAd--ClientSecret"
   value        = azuread_application_password.aadapppwd.value
 }
+
+resource "azuread_application_password" "localdevapppwd" {
+  count          = var.stage == "dev" ? 1 : 0
+  display_name   = "localdevapppwd"
+  application_id = azuread_application.aadapp.id
+  end_date       = timeadd(timestamp(), "17520h")
+}
+
+resource "azurerm_key_vault_secret" "localdevapppwd-secret" {
+  count        = var.stage == "dev" ? 1 : 0
+  key_vault_id = azurerm_key_vault.kv.id
+  name         = "LocalDevClientSecret"
+  value        = azuread_application_password.aadapppwd.value
+}
+
+resource "azuread_application" "postman-aadapp" {
+  display_name     = format("%s Postman %s", var.resource_prefix, var.stage)
+  identifier_uris  = []
+  sign_in_audience = "AzureADMyOrg"
+  api {
+    requested_access_token_version = 2
+  }
+
+  required_resource_access {
+    resource_app_id = azuread_application.aadapp.id
+
+    resource_access {
+      id   = random_uuid.random-uuid-forecast-read.result
+      type = "Role"
+    }
+  }
+}
+
+resource "azuread_service_principal" "postman-aadapp-sp" {
+  client_id = azuread_application.postman-aadapp.client_id
+
+  feature_tags {
+    enterprise = true
+  }
+}
